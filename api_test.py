6c2aa7187269f6ded79c0fa821cef0edd80c4107
Tom Higgins <thomas.higgins@manchester.ac.uk>
2016-03-08 10:32:55 +0000

    Added client_key

--- api_test.py
+++ api_test.py
@@ -7,7 +7,8 @@ import time
 verbose = False
 
 # System details
-server = "http://localhost:8080"
+server = "http://127.0.0.1:8080"
+client_key = "datavault-webapp"
 datapath = "/home/ubuntu/data/api-test-data"
 archivepath = "/home/ubuntu/data/api-test-archive"
 restoreDir = "restore"
@@ -22,21 +23,21 @@ def create_filestore(storageClass, label, path):
   if verbose:
     print("create_filestore : " + label)
   payload = {"storageClass": storageClass, "label": label, "properties":{"rootPath":path}}
-  headers = {'Content-type': 'application/json', 'X-UserID': username}
+  headers = {'Content-type': 'application/json', 'X-UserID': username, 'X-Client-Key': client_key}
   response = requests.post(server + '/datavault-broker/filestores', data=json.dumps(payload), headers=headers)
   return(response.json())
 
 def list_filestores():
   if verbose:
     print("list_filestores")
-  headers = {'Content-type': 'application/json', 'X-UserID': username}
+  headers = {'Content-type': 'application/json', 'X-UserID': username, 'X-Client-Key': client_key}
   response = requests.get(server + '/datavault-broker/filestores', headers=headers)
   return(response.json())
 
 def list_files(filestoreId):
   if verbose:
     print("list_files : " + filestoreId)
-  headers = {'Content-type': 'application/json', 'X-UserID': username}
+  headers = {'Content-type': 'application/json', 'X-UserID': username, 'X-Client-Key': client_key}
   response = requests.get(server + '/datavault-broker/files/' + filestoreId, headers=headers)
   return(response.json())
 
@@ -44,7 +45,7 @@ def create_archivestore(storageClass, label, path):
   if verbose:
     print("create_archivestore : " + label)
   payload = {"storageClass": storageClass, "label": label, "properties":{"rootPath":path}}
-  headers = {'Content-type': 'application/json', 'X-UserID': username}
+  headers = {'Content-type': 'application/json', 'X-UserID': username, 'X-Client-Key': client_key}
   response = requests.post(server + '/datavault-broker/archivestores', data=json.dumps(payload), headers=headers)
   return(response.json())
 
@@ -52,14 +53,14 @@ def create_vault(name, description, policyID, groupID):
   if verbose:
     print("create_vault : " + name)
   payload = {"name": name, "description": description, "policyID": policyID, "groupID": groupID}
-  headers = {'Content-type': 'application/json', 'X-UserID': username}
+  headers = {'Content-type': 'application/json', 'X-UserID': username, 'X-Client-Key': client_key}
   response = requests.post(server + '/datavault-broker/vaults', data=json.dumps(payload), headers=headers)
   return(response.json())
 
 def list_vaults():
   if verbose:
     print("list_vaults")
-  headers = {'Content-type': 'application/json', 'X-UserID': username}
+  headers = {'Content-type': 'application/json', 'X-UserID': username, 'X-Client-Key': client_key}
   response = requests.get(server + '/datavault-broker/vaults', headers=headers)
   return(response.json())
 
@@ -67,21 +68,21 @@ def create_deposit(vaultId, note, filePath):
   if verbose:
     print("create_deposit : " + note)
   payload = {"note": note, "filePath": filePath}
-  headers = {'Content-type': 'application/json', 'X-UserID': username}
+  headers = {'Content-type': 'application/json', 'X-UserID': username, 'X-Client-Key': client_key}
   response = requests.post(server + '/datavault-broker/vaults/' + vaultId + "/deposits", data=json.dumps(payload), headers=headers)
   return(response.json())
 
 def list_vault_deposits(vaultId):
   if verbose:
     print("list_vault_deposits : " + vaultId)
-  headers = {'Content-type': 'application/json', 'X-UserID': username}
+  headers = {'Content-type': 'application/json', 'X-UserID': username, 'X-Client-Key': client_key}
   response = requests.get(server + '/datavault-broker/vaults/' + vaultId + "/deposits", headers=headers)
   return(response.json())
   
 def get_deposit(vaultId, depositId):
   if verbose:
     print("get_deposit : " + vaultId + "/" + depositId)
-  headers = {'Content-type': 'application/json', 'X-UserID': username}
+  headers = {'Content-type': 'application/json', 'X-UserID': username, 'X-Client-Key': client_key}
   response = requests.get(server + '/datavault-broker/vaults/' + vaultId + "/deposits/" + depositId, headers=headers)
   return(response.json())
 
@@ -89,7 +90,7 @@ def create_restore(vaultId, depositId, note, restorePath):
   if verbose:
     print("create_restore : " + note)
   payload = {"note": note, "restorePath": restorePath}
-  headers = {'Content-type': 'application/json', 'X-UserID': username}
+  headers = {'Content-type': 'application/json', 'X-UserID': username, 'X-Client-Key': client_key}
   response = requests.post(server + '/datavault-broker/vaults/' + vaultId + "/deposits/" + depositId + "/restore", data=json.dumps(payload), headers=headers)
   return(response.json())
 
